ac7f68b4ad291bb271d0c8f61d81631f04638ed1
pahanez <pahanez@gmail.com>
2013-09-20 01:51:22 +0300

    alpha modifer

--- android/src/com/pahanez/wallpaper/cpu/CustomWallPaperService.java
+++ android/src/com/pahanez/wallpaper/cpu/CustomWallPaperService.java
@@ -6,6 +6,9 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.AlphaModifier;
+import org.andengine.entity.modifier.IEntityModifier;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.text.Text;
@@ -15,6 +18,8 @@ import org.andengine.opengl.font.FontFactory;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.util.adt.color.Color;
+import org.andengine.util.modifier.IModifier.DeepCopyNotSupportedException;
+import org.andengine.util.modifier.IModifier.IModifierListener;
 
 import android.graphics.Typeface;
 import android.util.DisplayMetrics;
@@ -48,8 +53,9 @@ public class CustomWallPaperService extends BaseLiveWallpaperService{
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws IOException {
 		 
-		mFont = FontFactory.create(getFontManager(), getTextureManager(), 512, 64, Typeface.createFromAsset(getAssets(), "fonts/arcade.ttf"), 26, true, android.graphics.Color.WHITE);
+		mFont = FontFactory.create(getFontManager(), getTextureManager(), 512, 512, Typeface.createFromAsset(WallApplication.getContext().getAssets(), "fonts/xspace.ttf"), 42, true, android.graphics.Color.WHITE);
 		getFontManager().loadFont(mFont);
+		mFont.load();
 		
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
@@ -58,9 +64,11 @@ public class CustomWallPaperService extends BaseLiveWallpaperService{
 	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
 			throws IOException {
 		mScene = new Scene();
-		Text text = new Text(100, 300, mFont, "Kimono", getVertexBufferObjectManager());
-		Rectangle rect = new Rectangle(300, 300, 200, 200, getVertexBufferObjectManager());
+		Text text = new Text(300, 300, mFont, "Ki12ffs____1241", getVertexBufferObjectManager());
+		text.registerEntityModifier(new AlphaModifier(2, 1, 0));
+		Rectangle rect = new Rectangle(0, 0, 200, 200, getVertexBufferObjectManager());
 		rect.setColor(Color.WHITE);
+		text.setColor(Color.WHITE);
 		mScene.attachChild(text);
 		mScene.attachChild(rect);
 		pOnCreateSceneCallback.onCreateSceneFinished(mScene);
